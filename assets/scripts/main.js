199bb3a3e528882cc9d259d785f559643fc34c59
Sanjib Roy <sanjibroy0098@gmail.com>
2020-03-29 22:48:11 +0530

    Funtionality complete. UI,timer and user login part is left

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,85 +2,114 @@ let container = document.querySelector('.container');
 let ul = document.querySelector('.all_options');
 let question = document.querySelector('.question');
 
+let allPlayer = JSON.parse(localStorage.getItem('allPlayer')) || [];
 
-let usersAnswer = 0;
+let usersAnswer = [];
 
-let questionArray = [
-    {
-        question : 'Which water body is to the south of West Bengal?',
-        answerIndex : 2,
-        options : ['Gulf of Kutch', 'Bay of Bengalcorrect', 'Gulf of Cambay', 'Red Sea']
-    },
-    
-    {
-        question : 'Where did the Portuguese have a factory in West Bengal?',
-        answerIndex : 3,
-        options : ['Barrackpore', 'Asansol', 'Jadavpur', 'Hooghly']
-    },
-
-    {
-        question : 'When did the French vacate Chandernagore?',
-        answerIndex : 0,
-        options : ['1949', '1961', '1975', '1954']
-    },
-
-    {
-        question : 'Who was West Bengal’s chief minister in 1980?',
-        answerIndex : 2,
-        options : ['Siddharth Shankar Ray', 'Asoke Sen', 'Jyoti Basu', 'Ajoy Bose']
-    },
-
-    {
-        question : 'Total Number of Lok Sabha seats in West Bengal?',
-        answerIndex : 2,
-        options : ['45', '43', '41', '47']
-    },
-
-    {
-        question : 'Who was the first person from West Bengal to win Bharat Ratna Award?',
-        answerIndex : 3,
-        options : ['Aruna Asaf Ali', 'Ravi Shankar', 'Satyajit Ray', 'Bidhan Chandra Roy']
-    }
-];
-
+let markColor  = `rgb(230, 226, 226)`;
 
 function getAnswer(event) {
 
+    let currentQuestion = quiz.currentQuestion;
     let option = document.querySelectorAll('.option');
-    let markColor  = 'rgb(230, 226, 226)';
-
-    option.forEach(el => el.style.backgroundColor = '#fff'); // Unmark
     
+    let id = event.target.dataset.id || event.target.parentNode.dataset.id;
+    let li = '';
 
     if(event.target.className == 'option') {
-        let radioBtn = event.target.children[0];
-        radioBtn.checked = true;
+        li = event.target;
+    } else {
+        li = event.target.parentNode;
+    }
+    
+    option.forEach(el => el.style.backgroundColor = '#fff'); // Unmark
+    
+    if(event.target.className == 'option' || event.target.hasAttribute('for')) {
+        let radioBtn = li.children[0];
+
+        radioBtn.checked = !radioBtn.checked;
         usersAnswer = radioBtn.value;   
         
-        event.target.style.backgroundColor = markColor;
-        
+        if(radioBtn.checked) {
+            li.style.backgroundColor = markColor;
+        } else {
+            li.style.backgroundColor = '#fff';
+        }
+    }
+
+    let obj = {
+        questionNo : currentQuestion,
+        usersAnswer : +usersAnswer,
+        correctAnswer : questionArray[currentQuestion].answerIndex
+    }
+
+    if(!player1.answers[currentQuestion]) {
+        player1.answers.push(obj);
     } else {
-        usersAnswer = event.target.parentNode.dataset.id;
-        event.target.parentNode.style.backgroundColor = markColor;
-    } 
+        player1.answers[currentQuestion].usersAnswer = +usersAnswer;
+    }
     
-    console.log(usersAnswer);
 }
 
 
 
 function render(event) {
+
+    player1.updateScore(quiz.currentQuestion)
+
+    if(event.target.classList.contains('next') ) {
+        if(quiz.currentQuestion + 1 < questionArray.length) {
+            quiz.currentQuestion += 1;
+            quiz.display();
+        }
+    } 
     
-    console.log(true);
-    if(event.target.className.split(' ').includes = 'next') {
-        this.currentQuestion++;
-    } else {
-        this.currentQuestion--;
+    else if(event.target.classList.contains('prev') ) {
+
+        if(quiz.currentQuestion - 1 >= 0) {
+            quiz.currentQuestion -= 1;
+            quiz.display();
+        }
+        
+    }
+}
+
+// Classes
+
+// Player Related
+
+class Player {
+    constructor(userName, allQuestions = [], score = 0) {
+        this.userName = userName;
+        this.allQuestions = allQuestions;
+        this.score = score;
+        this.id = Date.now() + Math.floor((Math.random() * 5)+1);
+        this.answers = []
+        
     }
 
-    quiz.display();
+    updateScore(currentQuestion) {
 
+        let current = this.answers[currentQuestion];
+
+        if(current) {
+            if(current.usersAnswer == current.correctAnswer) {
+                this.score += 20; 
+                
+            } else {
+                console.log(false);
+                this.score -= 10;
+            }
+        }
+    }
+
+    editUserName(newName = this.userName) {
+        this.userName = newName;
+    }
 }
+
+
+// Question Related
 
 class handleQuestion {
     constructor(allQuestions = [], currentQuestion) {
@@ -112,47 +141,57 @@ class handleQuestion {
     }
 
     display() {
-        // console.log(false);  
+      
         let current = this.allQuestions[this.currentQuestion];
+        let lis = '';
+        let btns = document.querySelector('.btns');
+        
         question.innerHTML = current.question;
 
-        let btns = document.querySelector('.btns');
-        btns.innerHTML = `<button class="skip btn">Skip</button>
+        btns.innerHTML = `<button class="prev btn">Previous</button>
                           <button class="next btn">Next</button>`;
                           
-
-        let lis = '';
-        
         for(let i = 0; i < current.options.length; i++) {
             
             lis += `<li class="option" data-id = ${i}>
                         <input type="radio" name = "option" id="option_${i}" value = "${i}">
-                        <label for="option_${i}">${current.options[i]}</label>
+                        <span for="option_${i}">${current.options[i]}</span>
                     </li>`;
         }
 
         ul.innerHTML = lis;
-        // setInterval(this.render,1000);
+        
         let option = document.querySelectorAll('.option');
 
-        option.forEach(el => {
+        option.forEach((el, index) => {
+            
             el.children[0].checked = false;
 
-            el.style.backgroundColor = '#fff';
             el.children[0].style.display = 'none'; 
 
             el.addEventListener('click', getAnswer);
 
+            if(player1.answers[this.currentQuestion]) {
+                if(player1.answers[this.currentQuestion].usersAnswer == el.dataset.id) {
+                    el.style.backgroundColor = markColor;
+                } 
+            }
         });
 
         let next = document.querySelector('.next');
-        let skip = document.querySelector('.skip');
+        let prev = document.querySelector('.prev');
        
         next.addEventListener('click', render);
-        skip.addEventListener('click', render);
-
+        prev.addEventListener('click', render);
     }
-
 }
 
+allPlayer.push(new Player('Sanjib',questionArray,0));
+
+localStorage.setItem('allPlayer',JSON.stringify(allPlayer));
+
+let player1 = allPlayer[allPlayer.length - 1];
 let quiz = new handleQuestion(questionArray,0);
+
+localStorage.setItem('quiz',JSON.stringify(quiz));
+
---
+++ assets/scripts/question.js
@@ -0,0 +1,37 @@
+let questionArray = [
+    {
+        question : 'Which water body is to the south of West Bengal?',
+        answerIndex : 1,
+        options : ['Gulf of Kutch', 'Bay of Bengal', 'Gulf of Cambay', 'Red Sea']
+    },
+    
+    {
+        question : 'Where did the Portuguese have a factory in West Bengal?',
+        answerIndex : 3,
+        options : ['Barrackpore', 'Asansol', 'Jadavpur', 'Hooghly']
+    },
+
+    {
+        question : 'When did the French vacate Chandernagore?',
+        answerIndex : 0,
+        options : ['1949', '1961', '1975', '1954']
+    },
+
+    {
+        question : 'Who was West Bengal’s chief minister in 1980?',
+        answerIndex : 2,
+        options : ['Siddharth Shankar Ray', 'Asoke Sen', 'Jyoti Basu', 'Ajoy Bose']
+    },
+
+    {
+        question : 'Total Number of Lok Sabha seats in West Bengal?',
+        answerIndex : 2,
+        options : ['45', '43', '41', '47']
+    },
+
+    {
+        question : 'Who was the first person from West Bengal to win Bharat Ratna Award?',
+        answerIndex : 3,
+        options : ['Aruna Asaf Ali', 'Ravi Shankar', 'Satyajit Ray', 'Bidhan Chandra Roy']
+    }
+];
