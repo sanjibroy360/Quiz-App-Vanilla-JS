daa0c2430848594271b794afa6acdafbb33cd106
Sanjib Roy <sanjibroy0098@gmail.com>
2020-04-05 23:57:19 +0530

    Modify Code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,8 @@
 let container = document.querySelector('.container');
 let ul = document.querySelector('.all_options');
 let question = document.querySelector('.question');
+let questionList = document.querySelector('.question_nos');
+let showListBtn = document.querySelector('.jump');
 
 let allPlayer = JSON.parse(localStorage.getItem('allPlayer')) || [];
 
@@ -9,6 +11,7 @@ let usersAnswer = [];
 let markColor  = `rgb(230, 226, 226)`;
 
 function getAnswer(event) {
+    event.stopPropagation();
 
     let currentQuestion = quiz.currentQuestion;
     let option = document.querySelectorAll('.option');
@@ -16,31 +19,32 @@ function getAnswer(event) {
     let id = event.target.dataset.id || event.target.parentNode.dataset.id;
     let li = '';
 
+    // debugger;
+
     if(event.target.className == 'option') {
         li = event.target;
+        
     } else {
         li = event.target.parentNode;
     }
     
-    option.forEach(el => el.style.backgroundColor = '#fff'); // Unmark
+    option.forEach(el => el.style.backgroundColor = 'transparent'); // Unmark
     
-    if(event.target.className == 'option' || event.target.hasAttribute('for')) {
+    // if(event.target.className == 'option' || event.target.hasAttribute('for')) {
         let radioBtn = li.children[0];
-
         radioBtn.checked = !radioBtn.checked;
+        // radioBtn.checked = !radioBtn.checked;
         usersAnswer = radioBtn.value;   
+        console.log(radioBtn.checked);
         
-        if(radioBtn.checked) {
-            li.style.backgroundColor = markColor;
-        } else {
-            li.style.backgroundColor = '#fff';
-        }
-    }
+        localStorage.setItem('allPlayer',JSON.stringify(allPlayer));
+    // }
 
     let obj = {
         questionNo : currentQuestion,
         usersAnswer : +usersAnswer,
-        correctAnswer : questionArray[currentQuestion].answerIndex
+        correctAnswer : questionArray[currentQuestion].answerIndex,
+        points : 0
     }
 
     if(!player1.answers[currentQuestion]) {
@@ -48,6 +52,18 @@ function getAnswer(event) {
     } else {
         player1.answers[currentQuestion].usersAnswer = +usersAnswer;
     }
+
+    if(radioBtn.checked) {
+        li.style.backgroundColor = markColor;
+       
+        
+    } else {
+        li.style.backgroundColor = 'transparent';
+        console.log(player1.answers);
+        player1.answers.splice(currentQuestion, 1);
+    }
+
+    localStorage.setItem('allPlayer',JSON.stringify(allPlayer));
     
 }
 
@@ -93,18 +109,27 @@ class Player {
         let current = this.answers[currentQuestion];
 
         if(current) {
+           
             if(current.usersAnswer == current.correctAnswer) {
-                this.score += 20; 
+                current.points = +20; 
                 
             } else {
-                console.log(false);
-                this.score -= 10;
+                
+                current.points = -10;
             }
         }
+        this.total()
+        localStorage.setItem('allPlayer',JSON.stringify(allPlayer));
+        console.log(allPlayer);
     }
 
     editUserName(newName = this.userName) {
         this.userName = newName;
+    }
+
+    total() {
+        this.score = this.answers.reduce( (acc, cv) => acc + cv.points ,0);
+        
     }
 }
 
@@ -137,7 +162,7 @@ class handleQuestion {
             options : options
         };
         this.allQuestions.splice(qno,1,editObj);
-        console.log('this', this.allQuestions);
+       
     }
 
     display() {
@@ -149,13 +174,14 @@ class handleQuestion {
         question.innerHTML = current.question;
 
         btns.innerHTML = `<button class="prev btn">Previous</button>
+                         
                           <button class="next btn">Next</button>`;
                           
         for(let i = 0; i < current.options.length; i++) {
             
             lis += `<li class="option" data-id = ${i}>
                         <input type="radio" name = "option" id="option_${i}" value = "${i}">
-                        <span for="option_${i}">${current.options[i]}</span>
+                        <span>${current.options[i]}</span>
                     </li>`;
         }
 
@@ -165,18 +191,42 @@ class handleQuestion {
 
         option.forEach((el, index) => {
             
-            el.children[0].checked = false;
+            // el.children[0].checked = false;
 
             el.children[0].style.display = 'none'; 
 
+            // el.children.forEach(item => item.addEventListener('click', getAnswer))
+            // ;
+
+            let eachLiItem = el.children;
+
             el.addEventListener('click', getAnswer);
+            Array.from(eachLiItem).forEach((item, index) => {
+                if(index <= 1) {
+                    item.addEventListener('click', getAnswer);
+                }
+            })
+
 
             if(player1.answers[this.currentQuestion]) {
                 if(player1.answers[this.currentQuestion].usersAnswer == el.dataset.id) {
                     el.style.backgroundColor = markColor;
+                    el.children[0].checked = true;
                 } 
             }
+
+            
         });
+
+        for(let i = 0; i < questionArray.length; i++) {
+            
+            let li = document.createElement('li');
+            li.setAttribute('data-qno',`${i}`);
+            li.className = 'qno';
+            li.innerHTML = `${i+1}`
+            
+            questionList.append(li);
+        }
 
         let next = document.querySelector('.next');
         let prev = document.querySelector('.prev');
@@ -185,6 +235,15 @@ class handleQuestion {
         prev.addEventListener('click', render);
     }
 }
+
+// function handleJump() {
+    
+// }
+
+
+
+
+// showListBtn.addEventListener('click', handleJump);
 
 allPlayer.push(new Player('Sanjib',questionArray,0));
 
